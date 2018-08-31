81d6fd55dc6ff2861887beab075ac434e03fef69
Vitao18 <victoronline9@gmail.com>
2018-08-31 16:28:56 -0300

    feat: incOrDec with a reset button

---
+++ 002-Inc-Or-Dec-Buttons/src/Main.elm
@@ -0,0 +1,44 @@
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+-- Model
+
+type alias Model = Int
+
+init : Model
+init =
+    0
+
+-- Update
+
+type Msg = Increment | Decrement | Reset
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment -> model + 1
+        Decrement -> model - 1
+        Reset     -> 0
+
+
+-- View
+
+view : Model -> Html Msg
+view model =
+    div []
+     [
+       div []
+         [ button [ onClick Decrement ] [ text "-" ]
+         , div [] [ text (String.fromInt model) ]
+         , button [ onClick Increment ] [ text "+" ]
+         ]
+     ,
+     button [ onClick Reset ] [ text "reset" ]
+     ]
+
+           
